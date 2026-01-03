4f769724db6736990a656b40c9815e4bd0c8db19
Abhinay Vamshi Potnuri <abhinayvamshi18@gmail.com>
2026-01-03 23:19:55 +0000

    Add Render domain to allowed hosts

--- brain_tumor/urls.py
+++ brain_tumor/urls.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("OK", status=200)
+
+urlpatterns = [
+    path('healthz/', health),
+    ...
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
